9cf81021216dccf431dd4a24e4638142a252a8b0
lucas <Rodriguesdesouzal28@gmail.com>
2024-04-13 10:09:33 -0300

    :art: setup folders

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,6 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function DashboardPage() {
+  return <Button variant={"secondary"}>DashboardPage</Button>;
+}
