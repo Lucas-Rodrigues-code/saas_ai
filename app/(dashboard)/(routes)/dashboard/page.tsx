5470e1d6284a2693dd984c1f57b2c4487ff31bfc
lucas <Rodriguesdesouzal28@gmail.com>
2024-04-13 14:45:05 -0300

    :sparkles: clerk Authentication

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,6 +1,12 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function DashboardPage() {
-  return <Button variant={"secondary"}>DashboardPage</Button>;
+  return (
+    <div>
+      <Button variant={"secondary"}>DashboardPage</Button>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 }
